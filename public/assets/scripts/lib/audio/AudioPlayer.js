da23e75f78f837b688cbbd8e4c00aa8c56b5f952
HyeBeen Byun <hyebeen2658@naver.com>
2022-10-02 10:55:36 +0900

    randomCountUp 완성

--- public/assets/scripts/lib/audio/AudioPlayer.js
+++ public/assets/scripts/lib/audio/AudioPlayer.js
@@ -1,40 +1,50 @@
 import { create } from "../dom/create.js";
+import { on } from "../dom/events.js";
 import { throwTypeError } from "../utils/throwError.js";
 import { isString } from "../utils/typeOf.js";
 
-export function AudioPlayer(source) {
-  if (!isString(source)) {
-    throwTypeError("source 인자는 오디오 경로이어야 합니다.");
-  }
+// Constructor, Prototype
+// audio 생성하고, 컨트롤(재생, 일시정지, 정지) 기능 부여
+// {
+//   function AudioPlayer(source) {
+//     if (!isString(source)) {
+//       throwTypeError("source 인자는 오디오 음원 경로(문자 값)이어야 합니다.");
+//     }
 
-  this._audio = create("audio", {
-    src: source,
-  });
-}
+//     this._audio = create("audio", {
+//       src: source,
+//     });
+//   }
 
-AudioPlayer.prototype.play = function () {
-  this._audio.play();
-};
+//   AudioPlayer.prototype.play = function () {
+//     this._audio.play();
+//   };
 
-AudioPlayer.prototype.loopPlay = function () {
-  this.play();
-  this._audio.addEventListener("ended", () => {
-    this.play();
-  });
-};
+//   AudioPlayer.prototype.loopPlay = function () {
+//     this.play();
+//     this._audio.addEventListener("ended", () => {
+//       this.play();
+//     });
+//   };
 
-AudioPlayer.prototype.pause = function () {
-  this._audio.pause();
-};
+//   AudioPlayer.prototype.pause = function () {
+//     this._audio.pause();
+//   };
 
-AudioPlayer.prototype.stop = function () {
-  this.pause();
-  this._audio.currentTime = 0;
-};
+//   AudioPlayer.prototype.stop = function () {
+//     this.pause();
+//     this._audio.currentTime = 0;
+//   };
+
+//   AudioPlayer.prototype.isPlaying = function () {
+//     return !this._audio.paused;
+//   };
+// }
 
 //# private
 // static
 
+// AudioPlayer class 문법을 사용해서 구현합니다.
 export class AudioPlayer {
   #audio = null;
 
