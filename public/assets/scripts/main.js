bebefbfbb7a15c4ad3b47ff93167a655f2c2bb03
HyeBeen Byun <hyebeen2658@naver.com>
2022-10-07 18:03:01 +0900

    snakeGame 수정

--- public/assets/scripts/main.js
+++ public/assets/scripts/main.js
@@ -8,8 +8,11 @@ import {
   addClass,
   removeClass,
   on,
+  hasClass,
+  getRandom,
 } from "../../lib/index.js";
 
+const SPEED_RATIO = 0.9; //.9 (0 생략가능)
 const SQUARE_ROWS = 10;
 const SQUARE_COLS = 10;
 const SQUARE_TOTAL = SQUARE_ROWS * SQUARE_COLS;
@@ -21,19 +24,23 @@ const GAME_ELEMENTS = {
 };
 
 const gridElement = $(`.${GAME_ELEMENTS.grid}`);
-const startButtonElement = $(`${".Button--start"}`);
-const stopButtonElement = $(`${".Button--stop"}`);
+const startButtonElement = $(`.${"Button--start"}`);
+const stopButtonElement = $(`.${"Button--stop"}`);
+const scoreDisplayElement = $(`.ScoreDisplay`);
+const SCORE_VALUE = 10;
 
 /* -------------------------------------------------------------------------- */
 /* 게임 변수                                                                    */
 /* -------------------------------------------------------------------------- */
 
 let squares = [];
-let snake = [2, 1, 0];
+let snake = [];
 let gameStopId;
 let speed = 1000;
 let isStarting = false;
 let direction = 1;
+let score = 0;
+let appleIndex;
 
 /* -------------------------------------------------------------------------- */
 /* 게임 함수                                                                    */
@@ -56,6 +63,19 @@ function drawSquares({ showGridNumbers = false } = {}) {
   //console.log(squares);
 }
 
+function drawApple() {
+  //스네이크와 겹치지 않는 그리드 위치 찾기 (do while )
+
+  do {
+    appleIndex = getRandom(SQUARE_TOTAL - 1);
+  } while (hasClass(squares[appleIndex], GAME_ELEMENTS.snake));
+
+  console.log(appleIndex);
+
+  //찾은 위치에 해당하는 squares DOM 요소에 애플 클래스 이름을 추가
+  isEatApple();
+}
+
 function drawSnake() {
   each(snake, (index) => {
     addClass(squares[index], GAME_ELEMENTS.snake);
@@ -71,49 +91,177 @@ function movingSnake() {
   //let headIndex = snake.unshift(snake[0] + 1); => 3만 출력함
 
   snake.unshift(snake[0] + direction);
+
   let headIndex = snake[0];
-  addClass(squares[headIndex], GAME_ELEMENTS.snake);
+  addHead();
+
+  return { headIndex, tailIndex };
 }
 
 function move() {
   if (isGameOver()) {
     return gameOver();
   }
-  movingSnake();
+
+  let { headIndex } = movingSnake();
+
+  //snake 헤드가 애플을 먹으면 처리
+
+  // hasClass(squares[headIndex], GAME_ELEMENTS.apple
+
+  if (hasClass(squares[headIndex], GAME_ELEMENTS.apple)) {
+    //에플은 그리드에서 사라져야 함
+    isEatApple();
+
+    //스네이크 꼬리가 길어져야 함
+    addTail();
+
+    // console.log({ snake });
+
+    //애플은 그리드의 임의 위치에 드로잉
+    drawApple();
+
+    //스코어 업
+    scoreUp();
+
+    //게임 난이도 업(게임 속도 향상)
+    speedUp();
+  }
+
   gameStopId = setTimeout(move, speed);
+}
+
+//--------------------------------------------------------------------
+
+/* refactoring function */
+
+function addHead() {
+  let headIndex = snake[0];
+  addClass(squares[headIndex], GAME_ELEMENTS.snake);
+}
+
+function isEatApple() {
+  let headIndex = snake[0];
+  if (hasClass(squares[headIndex], GAME_ELEMENTS.apple)) {
+    removeClass(squares[appleIndex], GAME_ELEMENTS.apple);
+  } else {
+    addClass(squares[appleIndex], GAME_ELEMENTS.apple);
+  }
+}
+
+function addTail() {
+  let { headIndex, tailIndex } = movingSnake();
+  addClass(squares[headIndex], GAME_ELEMENTS.snake);
+  snake.push(tailIndex);
+}
+
+//--------------------------------------------------------------------
+
+function scoreUp() {
+  score += SCORE_VALUE;
+  text(scoreDisplayElement, score);
+}
+
+function speedUp() {
+  speed = SPEED_RATIO * speed;
+  //console.log({ speed });
 }
 
 function isGameOver() {
   let headIndex = snake[0];
-  switch (direction) {
-    case -SQUARE_COLS:
-      if (headIndex + direction < 0) return true;
+  let nextHeadIndex = headIndex + direction;
 
+  switch (direction) {
+    //벽의 위에 뱀의 머리가 충돌
+    case -SQUARE_COLS:
+      if (nextHeadIndex < 0) return true;
+
+    //벽의 아래에 뱀의 머리가 충돌
     case SQUARE_COLS:
-      if (headIndex + direction >= SQUARE_TOTAL) return true;
+      if (nextHeadIndex >= SQUARE_TOTAL) return true;
 
     //벽의 왼쪽에 뱀의 머리가 충돌
     case -1:
       if (headIndex % SQUARE_COLS === 0) return true;
 
+    //벽의 오른쪽에 뱀의 머리가 충돌
     case 1:
       if (headIndex % SQUARE_COLS === SQUARE_COLS - 1) return true;
   }
-  //벽의 위에 뱀의 머리가 충돌
-  //벽의 아래에 뱀의 머리가 충돌
-  //벽의 왼쪽에 뱀의 머리가 충돌
-  //벽의 오른쪽에 뱀의 머리가 충돌
+
   //뱀의 몸통에 뱀의 머리가 충돌
+  if (hasClass(squares[nextHeadIndex], GAME_ELEMENTS.snake)) {
+    return true;
+  }
 
   return false;
 }
 
 function gameOver() {
-  alert("game over");
+  alert("Game Over");
   gameStop();
+
+  startButtonElement.disabled = false;
+  text(startButtonElement, "start");
+  stopButtonElement.disabled = true;
+
+  resetGameState();
+}
+
+function getRandomSnakeArray() {
+  let headIndex;
+  let restIndex;
+
+  do {
+    headIndex = getRandom(SQUARE_TOTAL - 1);
+    restIndex = headIndex % SQUARE_ROWS;
+    console.log({ restIndex });
+  } while (restIndex < SQUARE_ROWS - 7 || restIndex > SQUARE_ROWS - 3);
+
+  // console.log("-------------------끝", restIndex);
+
+  let bodyIndex = headIndex - 1;
+  let tailIndex = headIndex - 2;
+
+  return [headIndex, bodyIndex, tailIndex];
+}
+
+// loop(getRandomSnakeArray, 10);
+
+function resetSquare() {
+  each(squares, (square) =>
+    removeClass(square, GAME_ELEMENTS.snake, GAME_ELEMENTS.apple)
+  );
+}
+
+function resetScore() {
+  score = 0;
+  text(scoreDisplayElement, score);
+}
+
+function resetGameState() {
+  speed = 1000;
+  score = 0;
+  //pristine
+  //pure
+  //dirty
+
+  isStarting = false;
+  direction = 1;
+}
+
+function resetGame() {
+  resetSquare();
+  resetScore();
+
+  snake = getRandomSnakeArray();
+
+  drawSnake();
+  drawApple();
 }
 
 function gameStart() {
+  resetGame();
   gameReStart();
 }
 
@@ -128,7 +276,6 @@ function gameStop() {
 //showGridNumbers: true => grid안에 숫자 표시
 function init() {
   drawSquares({ showGridNumbers: true });
-  drawSnake();
 }
 
 init();
@@ -175,7 +322,7 @@ function handleKeyControl({ key }) {
       direction = 1;
       break;
   }
-  console.log(direction);
+  // console.log(direction);
 }
 
 on(startButtonElement, "click", handleGameStart);
